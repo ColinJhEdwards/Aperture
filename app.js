5bed59a9169d88641ad6d7ceea4e01be0ebabdbb
Colin Edwards <ColinJhEdwards@gmail.com>
2021-07-02 20:47:45 -0700

    added function that will display photos based off of search input

--- app.js
+++ app.js
@@ -1,13 +1,48 @@
 const auth = "563492ad6f91700001000001c52d87070c5547f29ee36618e00d5e4f";
 const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search-input");
-const submitButton = document.querySelector(".submit-btn");
+const form = document.querySelector(".search-form");
 let searchValue;
+
+//event listener
+searchInput.addEventListener("input", updateInput);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchPhotos(searchValue);
+});
+
+function updateInput(e) {
+  searchValue = e.target.value;
+}
 
 //fetch curated photos with api key and link
 async function curatedPhotos() {
   const dataFetch = await fetch(
     "https://api.pexels.com/v1/curated?per_page=15&page=1",
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    }
+  );
+  const data = await dataFetch.json();
+  // taking all photos returned from the fetch method and iterating over them with forEach and create a div that will display the image
+  data.photos.forEach((photo) => {
+    console.log(photo);
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `<img src=${photo.src.large}> </img>
+    <p>${photo.photographer}</p>
+    `;
+    gallery.appendChild(galleryImg);
+  });
+}
+
+async function searchPhotos(search) {
+  const dataFetch = await fetch(
+    `https://api.pexels.com/v1/search?query=${search}+query&per_page=15&page=1`,
     {
       method: "GET",
       headers: {
