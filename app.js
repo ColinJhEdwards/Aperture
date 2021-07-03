8fca0b74a8cfc3121452e497f73925c43618927a
Colin Edwards <ColinJhEdwards@gmail.com>
2021-07-02 20:56:44 -0700

    refactored some functions for less repetition by creating a function for fetching, and one for creating divs for the images

--- app.js
+++ app.js
@@ -15,53 +15,45 @@ function updateInput(e) {
   searchValue = e.target.value;
 }
 
-//fetch curated photos with api key and link
-async function curatedPhotos() {
-  const dataFetch = await fetch(
-    "https://api.pexels.com/v1/curated?per_page=15&page=1",
-    {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        Authorization: auth,
-      },
-    }
-  );
+//function that can be used for any api fetch
+async function fetchApi(url) {
+  const dataFetch = await fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: auth,
+    },
+  });
   const data = await dataFetch.json();
+  return data;
+}
+
+function generatePictures(data) {
   // taking all photos returned from the fetch method and iterating over them with forEach and create a div that will display the image
   data.photos.forEach((photo) => {
     console.log(photo);
     const galleryImg = document.createElement("div");
     galleryImg.classList.add("gallery-img");
     galleryImg.innerHTML = `<img src=${photo.src.large}> </img>
-    <p>${photo.photographer}</p>
-    `;
+          <p>${photo.photographer}</p>
+          `;
     gallery.appendChild(galleryImg);
   });
 }
 
-async function searchPhotos(search) {
-  const dataFetch = await fetch(
-    `https://api.pexels.com/v1/search?query=${search}+query&per_page=15&page=1`,
-    {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        Authorization: auth,
-      },
-    }
+async function curatedPhotos() {
+  const data = await fetchApi(
+    "https://api.pexels.com/v1/curated?per_page=15&page=1"
   );
-  const data = await dataFetch.json();
-  // taking all photos returned from the fetch method and iterating over them with forEach and create a div that will display the image
-  data.photos.forEach((photo) => {
-    console.log(photo);
-    const galleryImg = document.createElement("div");
-    galleryImg.classList.add("gallery-img");
-    galleryImg.innerHTML = `<img src=${photo.src.large}> </img>
-    <p>${photo.photographer}</p>
-    `;
-    gallery.appendChild(galleryImg);
-  });
+  console.log(data);
+  generatePictures(data);
+}
+
+async function searchPhotos(search) {
+  const data = await fetchApi(
+    `https://api.pexels.com/v1/search?query=${search}+query&per_page=15&page=1`
+  );
+  generatePictures(data);
 }
 
 curatedPhotos();
