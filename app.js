48f24a983bd9292c8488bafe5c12e88815fa0991
Colin Edwards <ColinJhEdwards@gmail.com>
2021-07-02 20:36:08 -0700

    created fetch function that will retrieve photos and displayed them on page

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 const auth = "563492ad6f91700001000001c52d87070c5547f29ee36618e00d5e4f";
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const submitButton = document.querySelector(".submit-btn");
+let searchValue;
+
+//fetch curated photos with api key and link
+async function curatedPhotos() {
+  const dataFetch = await fetch(
+    "https://api.pexels.com/v1/curated?per_page=15&page=1",
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    }
+  );
+  const data = await dataFetch.json();
+  // taking all photos returned from the fetch method and iterating over them with forEach and create a div that will display the image
+  data.photos.forEach((photo) => {
+    console.log(photo);
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `<img src=${photo.src.large}> </img>
+    <p>${photo.photographer}</p>
+    `;
+    gallery.appendChild(galleryImg);
+  });
+}
+
+curatedPhotos();
